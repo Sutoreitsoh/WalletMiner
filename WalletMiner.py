d422564cfe3d3178d836120a9d202d22f2eabec6
z5989 <71980857+z5989@users.noreply.github.com>
2022-05-27 19:47:24 +0200

    Add files via upload

---
+++ WalletMiner.py
@@ -0,0 +1,38 @@
+import string, random, colorama
+
+
+from colorama import Fore, Back, Style
+
+
+colorama.init()
+
+
+
+text = " 0xlsie4d"
+
+
+address=input('''
+      ** **               **                               
+     /**/**              /**      **   **  **   **  **   **
+     /**/**        ******/**  ** //** **  //** **  //** ** 
+     /**/**       **//// /** **   //***    //***    //***  
+     /**/**      //***** /****     /**      /**      /**   
+ **  /**/**       /////**/**/**    **       **       **    
+
+//***** /******** ****** /**//**  **       **       **                  
+
+
+ /////  //////// //////  //  //  //       //       //    ''')
+ 
+
+print()
+print()
+print()
+address=input("  WALLET : ")
+
+
+
+
+while True:
+
+    print((Fore.WHITE + " [+] ") + ''.join(random.choice(string.ascii_letters + string.digits) for i in range(52)) + (Fore.RED + " - 0.0000 ETH"))
